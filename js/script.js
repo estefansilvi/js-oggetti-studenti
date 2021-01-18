85939baca0cd6fba8f506f3fb1c892c8bf46bbd9
Stefania <estefansilvi@gmail.com>
2021-01-18 16:50:00 +0100

    script corretto e funzionante

--- js/script.js
+++ js/script.js
@@ -12,15 +12,15 @@ for ( var key in studente){
 
 // creo un array di oggetti( studenti). e stampo per ognuno le sue proprietà
 var studenti = [
-  studenteUno = {
+   {
     'nome':'Dante',
     'cognome':'Alighieri'
   },
-  studenteDue = {
+   {
     'nome':'Giacomo',
     'cognome':'Leopardi'
   },
-  studenteTre = {
+  {
     'nome':'Alessandro',
     'cognome':'Manzoni'
   }
@@ -28,8 +28,8 @@ var studenti = [
 
 for(var i = 0; i < studenti.length; i++){
   console.log('questo è lo studente n:' + (i+1));
-  for (var key in studenti){
-  console.log(studenti[key]);
+  for (var key in studenti[i]){
+  console.log(studenti[i][key]);
   }
 }
 
@@ -40,7 +40,7 @@ var nuovoStudente = {}
   nuovoStudente.cognome = prompt('cognome nuovo studente?')
   nuovoStudente.anni = prompt('anni del nuovo studente?')
 
-for(var key in studenti){
+// stampiamo l'array con il nuovo studente aggiunto
   studenti.push(nuovoStudente);
-}
+
  console.log(studenti);
